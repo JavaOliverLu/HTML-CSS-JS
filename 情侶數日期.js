9ce55b9f6ab955cea9849115c85d3edc8c556f92
JavaOliverLu <70323169+JavaOliverLu@users.noreply.github.com>
2020-12-27 11:38:18 +0800

    love//

---
+++ 情侶數日期.js
@@ -0,0 +1,123 @@
+
+
+var d1=new Date('2020-7-6');
+var d2=new Date();
+var d3=(d2-d1)/86400000;
+document.write("我們交往"+Math.floor((d3))+"天");
+    switch (Math.floor((d3))) {
+  case 10:
+    document.write(",10天快樂");
+            var i;
+for (i = 0; i < 10; i++) {
+  alert("10天快樂");
+}
+    break;
+  case 20:
+   document.write(",20天快樂");
+            var i;
+for (i = 0; i < 20; i++) {
+  alert("20天快樂");
+}
+    break;
+  case 30:
+    document.write(",周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("1周月快樂");
+}
+    break;
+  case 50:
+   document.write(",50天快樂");
+               var i;
+for (i = 0; i < 50; i++) {
+  alert("50天快樂");
+}
+    break;
+  case 60:
+   document.write(",2周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("2周月快樂");
+}
+    break;
+case 90:
+    document.write(",3周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("3周月快樂");
+}
+    break;
+case 100:
+  document.write(",100天快樂");
+               var i;
+for (i = 0; i < 100; i++) {
+  alert("100天快樂,從現在起 你可以叫我老公,我可以叫你老婆");
+}
+    break;
+case 120:
+    document.write(",4周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("4周月快樂");
+}
+    break;
+case 150:
+  document.write(",5周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("5周月快樂");
+}
+    break;
+case 180:
+   document.write(",半年了");
+    break;
+case 200:
+    document.write(",200天快樂");
+               var i;
+for (i = 0; i < 100; i++) {
+  alert("200天快樂");
+}
+    break;
+case 210:
+  document.write(",7周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("7周月快樂");
+}
+    break;
+case 240:
+   document.write(",8周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("8周月快樂");
+}
+    break;
+case 250:
+    document.write(",250天快樂");
+    break;
+case 270:
+  document.write(",9周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("9周月快樂");
+}
+    break;
+case 300:
+      document.write(",300天快樂");
+               var i;
+for (i = 0; i < 100; i++) {
+  alert("300天快樂");
+}
+    break;
+case 330:
+   document.write(",11周月快樂");
+            var i;
+for (i = 0; i < 30; i++) {
+  alert("11周月快樂");
+}
+    break;
+case 365:
+   document.write(",1周年快樂");
+    break;
+}
+
